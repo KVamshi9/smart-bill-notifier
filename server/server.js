fa038c9ce0d2e321863e740002b3a28baf003abd
Vamshi K <159602569+KVamshi9@users.noreply.github.com>
2026-02-16 07:52:41 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -173,7 +173,7 @@ cron.schedule("* * * * *", async () => {
 })
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port", PORT)
